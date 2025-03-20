24055d403774ad7a234928549a80f7c5de516480
syaquiii <msyauqioqi@gmail.com>
2025-03-20 14:03:18 +0700

    Project Structure Setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./*.{html,php,js}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "1rem",
+      },
+    },
+  },
+  plugins: [],
+};
